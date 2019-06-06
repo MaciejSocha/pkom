5e53aa61f0fdd3a8f23f742d9be0875ad4421321
MaciejSocha <maciek.socha1@gmail.com>
2019-06-06 23:31:35 +0200

    Showing burger data

--- Zadanie5/src/main/java/GUI/Controller.java
+++ Zadanie5/src/main/java/GUI/Controller.java
@@ -1,5 +1,7 @@
 package GUI;
 
+import generated.Burger;
+import generated.Burgerownia;
 import generated.MiesnoscBase;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
@@ -8,8 +10,13 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import logic.XMLOperations;
 
+import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -42,10 +49,25 @@ public class Controller implements Initializable {
     @FXML
     public DatePicker data = new DatePicker();
 
+    private Burgerownia burgerownia;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        listView.setItems(FXCollections.observableArrayList("AAA", "BBB"));
         miesnosc.getItems().addAll(MiesnoscBase.values());
+        try {
+            XMLOperations.readFromXML();
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        burgerownia = XMLOperations.burgerownia;
+        ArrayList<String> nazwy = new ArrayList<>();
+        for (Burger burger:
+             burgerownia.getListaBurgerów().getBurger()) {
+            nazwy.add(burger.getNazwa());
+        }
+        listView.setItems(FXCollections.observableArrayList(nazwy));
     }
 
     public void clear(){
@@ -77,5 +99,32 @@ public class Controller implements Initializable {
 
     public void toTXT(){
         System.out.println("robie txt");
+    }
+
+    public void select(){
+        System.out.println("Wybrano");
+        for (Burger burger:
+                burgerownia.getListaBurgerów().getBurger()) {
+            if (burger.getNazwa().equals(listView.getSelectionModel().getSelectedItem())){
+                nazwa.setText(burger.getNazwa());
+                cena.setText(String.valueOf(burger.getCena().getValue()));
+                kalorycznosc.setText(String.valueOf(burger.getKaloryczność().getValue()));
+                skladnik1.setText(burger.getSkladnik1());
+                skladnik2.setText(burger.getSkladnik2());
+                skladnik3.setText(burger.getSkladnik3());
+                skladnik4.setText(burger.getSkladnik4());
+                skladnik5.setText(burger.getSkladnik5());
+                skladnik6.setText(burger.getSkladnik6());
+                skladnik7.setText(burger.getSkladnik7());
+                skladnik8.setText(burger.getSkladnik8());
+                if (burger.getMięsność().name().equals("MIĘSNY"))
+                    miesnosc.getSelectionModel().select(0);
+                if (burger.getMięsność().name().equals("WEGETARIAŃSKI"))
+                    miesnosc.getSelectionModel().select(1);
+                if (burger.getMięsność().name().equals("WEGAŃSKI"))
+                    miesnosc.getSelectionModel().select(2);
+                data.setValue(LocalDate.parse(String.valueOf(burger.getDataWprowadzenia())));
+            }
+        }
     }
 }
